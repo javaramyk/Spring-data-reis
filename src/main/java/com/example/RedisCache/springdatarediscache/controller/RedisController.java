a41a25935684f116819996da9c56a8126afc0c7f
karanara <karanara@mail.uc.edu>
2024-03-26 06:03:07 -0400

    added caching logic of cacheable and cacheevict

--- src/main/java/com/example/RedisCache/springdatarediscache/controller/RedisController.java
+++ src/main/java/com/example/RedisCache/springdatarediscache/controller/RedisController.java
@@ -3,6 +3,9 @@ package com.example.RedisCache.springdatarediscache.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,7 @@ import com.example.RedisCache.springdatarediscache.repository.ProductDAO;
 
 @RestController
 @RequestMapping("/redis/product")
+@EnableCaching
 public class RedisController {
 
 	@Autowired
@@ -31,10 +35,12 @@ public class RedisController {
 		return productDAO.findAll();
 	}
 	@GetMapping("/findAll/{id}")
+	@Cacheable(key="#id",value="Product",unless="#result.price > 1000")
 	public Product findProductById(@PathVariable int id) {
 		return productDAO.findProductById(id);
 	}
 	@DeleteMapping("/delete/{id}")
+	@CacheEvict(key="#id",value="Product")
 	public String remove(@PathVariable int id) {
 		return productDAO.deleteProduct(id);
 	}
